539e596eded1e9a9ef76a45120b2fb9164da8629
Raymond Aung <raymond.psaung@gmail.com>
2023-04-18 16:54:54 +1000

    Initialize posthog in MyWoker

--- lib/worker.rb
+++ lib/worker.rb
@@ -1,10 +1,26 @@
+require 'posthog-ruby'
+
 class MyWorker
   include Sneakers::Worker
+  attr_reader :posthog
+
+  def initialize(...)
+    super(...)
+    @posthog = PostHog::Client.new({
+      api_key: ENV['POSTHOG_API_KEY'],
+      personal_api_key: ENV['POSTHOG_PERSONAL_KEY'],
+      host: ENV['POSTHOG_HOST'],
+      feature_flags_polling_interval: 10,
+      on_error: Proc.new { |status, msg| print msg }
+    })
+    @posthog.logger.level = Logger::DEBUG
+  end
+
   from_queue 'myworker'
 
   def work(data)
     puts "executing at #{Time.now}"
-    puts $posthog.get_feature_flag('test-feature', 1)
+    puts @posthog.get_feature_flag('test-feature', 1)
     ack!
   end
 end
--- workers.rb
+++ workers.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 require 'sneakers'
 require 'sneakers/runner'
 require './lib/worker'
-require 'posthog-ruby'
 
 Sneakers.configure(
   :amqp => ENV['RABBITMQ_URL'],
@@ -10,16 +9,7 @@ Sneakers.configure(
   :log => STDOUT,
   workers: 1
 )
-Sneakers.logger.level = ::Logger::INFO
-
-$posthog = PostHog::Client.new({
-  api_key: ENV['POSTHOG_API_KEY'],
-  personal_api_key: ENV['POSTHOG_PERSONAL_KEY'],
-  host: ENV['POSTHOG_HOST'],
-  feature_flags_polling_interval: 10,
-  on_error: Proc.new { |status, msg| print msg }
-})
-$posthog.logger.level = Logger::DEBUG
+Sneakers.logger.level = ::Logger::DEBUG
 
 r = Sneakers::Runner.new([ MyWorker ])
 r.run
